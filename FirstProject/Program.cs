6f6eb07dfc017dae2662192f0236cadbbb5bceab
ABDURA <abdura52.uz@gmail.com>
2023-07-11 07:08:52 -0700

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+while(true)
+{
+    Random random = new Random();
+    Console.Write(random.Next(10, 99) + " ");
+}
