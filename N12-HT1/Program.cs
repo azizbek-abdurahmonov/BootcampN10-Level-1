1d64b43e00d0e56b0a6e8a5615cc40d2e4acd663
ABDURA <abdura52.uz@gmail.com>
2023-07-26 15:05:36 +0500

    n12 hts added

--- N12-HT1/Program.cs
+++ N12-HT1/Program.cs
@@ -1,70 +1,113 @@
-﻿var user1 = new User();
-try
-{
-    user1.FullName = "Abdurahmonov Azizbek Alisherovich";
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+﻿// 1 - usul
+var user1 = new User();
+user1.ism = "Azizbek";
+user1.sharif = "Alisherovich";
+user1.familiya = "Abdurahmonov";
+
+Console.WriteLine($"1- user: {user1.FullName}");
 
 var user2 = new User();
-try
-{
-    user2.FullName = "Abdurahmonov Azizbek Alisherovich";
+user2.ism = "Azizbek";
+user2.sharif = "Alisherovich";
+user2.familiya = "Abdurahmonov";
 
-}
-catch(Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+Console.WriteLine($"1- user: {user2.FullName}");
 
-Console.WriteLine(user1.Equals(user2)); // True - ularning qiymatlari bir xil
+Console.WriteLine(user1.Equals(user2));
+
 
 
 public class User
 {
-    private string _name;
-    private string _firstName;
-    private string _lastName;
-    private string _fullName;
-
-
-    public string FullName
-    {
-        get
-        {
-            return _fullName;
-        }
-        set
-        {
-            if (string.IsNullOrWhiteSpace(value))
-                throw new Exception("Hech narsa kiritmadingiz!");
-
-            if (value.Split().Length != 3)
-                throw new Exception("Ism, Familiya va Sharif noto'g'ri formatda kiritildi!");
-
-            var names = value.Split();
-            _name = names[1];
-            _firstName = names[2];
-            _lastName = names[0];
-            _fullName = value;
-        }
-    }
+    public string ism { get; set; }
+    public string sharif { get; set; }
+    public string familiya { get; set; }
+    public string FullName => $"{familiya} {ism} {sharif}";
 
     public override int GetHashCode()
     {
-        return this._name.GetHashCode() + this._firstName.GetHashCode() + this._lastName.GetHashCode();
+        return this.ism.GetHashCode() + this.sharif.GetHashCode() + this.familiya.GetHashCode();
     }
 
     public override bool Equals(object? obj)
     {
         if (obj is User user)
-        {
             return user.GetHashCode() == this.GetHashCode();
-        }
 
         return false;
     }
-
 }
+
+
+// 2 - usul
+
+//var user1 = new User();
+//try
+//{
+//    user1.FullName = "Abdurahmonov Azizbek Alisherovich";
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//var user2 = new User();
+//try
+//{
+//    user2.FullName = "Abdurahmonov Azizbek Alisherovich";
+
+//}
+//catch(Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
+
+//Console.WriteLine(user1.Equals(user2)); // True - ularning qiymatlari bir xil
+
+
+//public class User
+//{
+//    private string _name;
+//    private string _firstName;
+//    private string _lastName;
+//    private string _fullName;
+
+
+//    public string FullName
+//    {
+//        get
+//        {
+//            return _fullName;
+//        }
+//        set
+//        {
+//            if (string.IsNullOrWhiteSpace(value))
+//                throw new Exception("Hech narsa kiritmadingiz!");
+
+//            if (value.Split().Length != 3)
+//                throw new Exception("Ism, Familiya va Sharif noto'g'ri formatda kiritildi!");
+
+//            var names = value.Split();
+//            _name = names[1];
+//            _firstName = names[2];
+//            _lastName = names[0];
+//            _fullName = value;
+//        }
+//    }
+
+//    public override int GetHashCode()
+//    {
+//        return this._name.GetHashCode() + this._firstName.GetHashCode() + this._lastName.GetHashCode();
+//    }
+
+//    public override bool Equals(object? obj)
+//    {
+//        if (obj is User user)
+//        {
+//            return user.GetHashCode() == this.GetHashCode();
+//        }
+
+//        return false;
+//    }
+
+//}
